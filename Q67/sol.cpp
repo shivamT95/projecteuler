9cfeac576c2ccedf0ff763fa87a3a50506099e61
manu4rhyme <manu3rhyme@gmail.com>
2018-04-22 15:48:54 +0530

    Q18 & Q67

---
+++ Q67/sol.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int DP[101][101];
+long long int triangle[101][101];
+
+int main()
+{
+	int N=100,i,j;
+	for(i = 1; i <= N; ++i)
+	{
+		for(j = 1; j <= i; ++j)
+			cin >> triangle[i][j];
+	}
+
+	DP[1][1] = triangle[1][1];
+	for(i = 2; i <= N; ++i)
+	{
+		DP[i][1] = DP[i-1][1] + triangle[i][1];
+		for(j = 2; j < N; ++j)
+		{
+			DP[i][j] = triangle[i][j] + max(DP[i-1][j], DP[i-1][j-1]);
+		}
+		DP[i][j] = DP[i-1][j-1]+triangle[i][j];
+	}
+	long long int ans = -1;
+	for(j = 1; j <= N; ++j)
+		ans = max(ans, DP[N][j]);
+
+	cout << ans;
+}
