003d95442fbcbcc648dd1314d640b56520ee22fd
Shivam Tyagi <shivam.tyagi.apm13@iitbhu.ac.in>
2018-04-22 16:26:30 +0530

    problem 22 and 23

---
+++ Q22/sol.py
@@ -0,0 +1,10 @@
+fo = open("inp.txt",'r')
+l = [x[1:-1] for x in fo.readline().split(',')]
+l.sort()
+a = 0
+print(l)
+for i in range(len(l)):
+	sc = sum([(ord(c)-ord('A')+1) for c in l[i]])
+	a += sc*(i+1)
+print(a)
+	
---
+++ Q23/sol.py
@@ -0,0 +1,29 @@
+divs = {}
+def sm(x):
+	s = 0
+	for i in range(2,int(x**0.5)+1):
+		if x%i == 0:
+			s += i
+			s += x/i
+			if i*i == x:
+				s -= i
+	return s+1
+print(sm(12))
+ans = 0
+ab = {}
+n = 28124
+for i in range(1,28124):
+	if sm(i)>i:
+		ab[i] = 1
+print(ab.keys()[:20])
+for i in range(1,n+1):
+	f = 0
+	for j in ab.keys():
+		if j>= i:
+			break
+		if (i-j) in ab:
+			f = 1
+	if f == 0:
+		print(i)
+		ans += i	
+print(ans)	
