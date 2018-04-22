ab1cb44d6304f76b1d02341d58299af17769739b
Shivam Tyagi <shivam.tyagi.apm13@iitbhu.ac.in>
2018-04-22 15:42:32 +0530

    scripts and problem 21

---
+++ Q21/sol.py
@@ -0,0 +1,20 @@
+divs = {}
+def sm(x):
+	s = 0
+	for i in range(2,int(x**0.5)):
+		if x%i == 0:
+			s += i
+			s += x/i
+			if i*i == x:
+				s -= i
+	return s+1
+print(sm(220))
+for i in range(10001):
+	divs[i] = sm(i)
+ans = 0
+for i in range(10001):
+	for j in range(10001):
+		if divs[i] == j and divs[j] == i and i!=j:
+			ans += i
+			print(i)
+print(ans)	
