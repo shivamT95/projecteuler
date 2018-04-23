964837f973f35cdf74a8cc584ac90fea53cf24e5
manu4rhyme <manu3rhyme@gmail.com>
2018-04-23 20:19:28 +0530

    Problem 38

---
+++ Q38/sol.py
@@ -0,0 +1,20 @@
+def check(n):
+	if(n < 100000000 or n > 999999999):
+		return False
+	st = set()
+	while(n):
+		st.add(n%10)
+		n = n//10
+	return (len(st) == 9 and (0 not in st))
+ans = -1
+for n in range(2,10):
+	for i in range(1,10000000000):
+		pdt = ""
+		for j in range (1,n+1):
+			pdt = pdt+str(i*j)
+		pdt = int(pdt)
+		if(pdt > 999999999):
+			break
+		if(check(pdt)):
+			ans = max(ans,pdt)
+print(ans)
